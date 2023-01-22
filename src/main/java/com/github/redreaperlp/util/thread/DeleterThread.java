3545b135efc4296fe24cbbd4d1776d2f9777f630
Raphael Kuschmierz <fiffykuschmierz@gmail.com>
2023-01-22 02:03:31 +0100

    Update DeleterThread.java

--- src/main/java/com/github/redreaperlp/util/thread/DeleterThread.java
+++ src/main/java/com/github/redreaperlp/util/thread/DeleterThread.java
@@ -3,6 +3,7 @@ package com.github.redreaperlp.util.thread;
 import com.github.redreaperlp.enums.CommandEn;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
@@ -11,16 +12,14 @@ import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class DeleterThread implements Runnable {
     public static List<String> ids = new ArrayList<>();
+    public static List<String> runningIds = new ArrayList<>();
     private SlashCommandInteractionEvent e;
     private InteractionHook hook;
-    private Message message;
-    private int delay;
+    private int cleared;
     private int action;
 
     Object lock = new Object();
@@ -31,77 +30,81 @@ public class DeleterThread implements Runnable {
         this.e = e;
     }
 
-    public DeleterThread(Message hook, int delay, SlashCommandInteractionEvent e) {
-        this.e = e;
-        this.message = hook;
-        this.delay = delay;
-        this.action = 1;
-    }
-
     @Override
     public void run() {
         synchronized (lock) {
-            if (action == 0) {
+            if (!runningIds.contains(e.getChannel().getId())) {
                 boolean clearUserMSG = false;
-
-                OptionMapping userOption = e.getOption(CommandEn.CLEAR.optionName(1));
-                if (userOption != null) {
-                    clearUserMSG = true;
-                }
-
+                boolean zero = false;
                 int toClear = e.getOption(CommandEn.CLEAR.optionName(0)).getAsInt();
-                MessageHistory his = e.getChannel().getHistoryAround(e.getChannel().getLatestMessageId(), 100).complete();
-                List<Message> messagesHis = his.getRetrievedHistory();
-                List<Message> messagesToDel = new ArrayList<>();
-                int cleared = 0;
-                if (clearUserMSG) {
-                    for (Message msg : messagesHis) {
-                        if (!isReply(msg)) {
-                            if (msg.getAuthor().equals(userOption.getAsUser())) {
-                                messagesToDel.add(msg);
-                                cleared++;
+                if (toClear == 0) {
+                    MessageEmbed equalsZero = new EmbedBuilder().setTitle("\"0\" is not a operatable number!").setThumbnail("https://discordapp.com/channels/591985618469257218/1060601917878829226/1066511720383778876").setColor(0xff0000).build();
+                    e.getHook().sendMessageEmbeds(equalsZero).complete();
+                    zero = true;
+                }
+                if (!zero) {
+                    OptionMapping userOption = e.getOption(CommandEn.CLEAR.optionName(1));
+                    if (userOption != null) {
+                        clearUserMSG = true;
+                    }
+
+                    MessageHistory his = e.getChannel().getHistoryAround(e.getChannel().getLatestMessageId(), 100).complete();
+                    List<Message> messagesHis = his.getRetrievedHistory();
+                    List<Message> messagesToDel = new ArrayList<>();
+                    int cleared = 0;
+                    if (clearUserMSG) {
+                        for (Message msg : messagesHis) {
+                            if (!isReply(msg)) {
+                                if (msg.getAuthor().equals(userOption.getAsUser())) {
+                                    messagesToDel.add(msg);
+                                    cleared++;
+                                }
+                                if (cleared == toClear) {
+                                    break;
+                                }
                             }
-                            if (cleared == toClear) {
-                                break;
+                        }
+                    } else {
+                        for (Message msg : messagesHis) {
+                            if (!isReply(msg)) {
+                                if (!msg.isEphemeral()) {
+                                    cleared++;
+                                }
+                                messagesToDel.add(msg);
+                                if (cleared == toClear) {
+                                    break;
+                                }
                             }
                         }
                     }
-                } else {
-                    for (Message msg : messagesHis) {
-                        if (!isReply(msg)) {
-                            if (!msg.isEphemeral()) {
-                                cleared++;
-                            }
-                            messagesToDel.add(msg);
-                            if (cleared == toClear) {
-                                break;
-                            }
-                        }
+                    if (cleared == 0) {
+
+                        e.getHook().sendMessageEmbeds(new EmbedBuilder().setTitle("There were no messages to Clear").setColor(0xff0000).build()).complete();
+                    } else {
+
+                        runningIds.add(e.getChannel().getId());
+                        e.getChannel().purgeMessages(messagesToDel);
+                        e.getHook().sendMessageEmbeds(new EmbedBuilder().setTitle("Cleared " + cleared + " messages!").setColor(0x00ff00).build()).complete();
                     }
                 }
-                e.getChannel().purgeMessages(messagesToDel);
-                new DeleterThread(e.getHook().sendMessageEmbeds(new EmbedBuilder().setTitle("Cleared " + cleared + " messages!").setColor(0x00ff00).build()).complete(), 5, e).run();
-            } else if (action == 1) {
                 e.getChannel().getHistory().retrievePast(1).queue(messages -> {
                     if (messages.size() > 0) {
-                        Message message = messages.get(0);
-                        ids.add(message.getId());
-                        CompletableFuture.delayedExecutor(delay, TimeUnit.SECONDS).execute(() -> {
-                            message.delete().queue();
-                            ids.remove(message.getId());
+                        ids.add(messages.get(0).getId());
+                        CompletableFuture.delayedExecutor(5, TimeUnit.SECONDS).execute(() -> {
+                            messages.get(0).delete().queue();
+                            ids.remove(messages.get(0).getId());
+                            runningIds.remove(e.getChannel().getId());
                         });
                     }
                 });
+            } else {
+                e.getHook().sendMessageEmbeds(new EmbedBuilder().setTitle("There is already a Delete request Running!").setColor(0xff0000).build()).complete();
             }
         }
     }
 
+
     public boolean isReply(Message msg) {
-        for (String id : DeleterThread.ids) {
-            if (msg.getId().equals(id)) {
-                return true;
-            }
-        }
-        return false;
+        return ids.contains(msg.getId());
     }
 }
